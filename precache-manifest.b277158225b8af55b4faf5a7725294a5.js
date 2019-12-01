b6f8734817219f552d105ff1035e18724f52db79
Nicholas Monacelli <nicholas@Nicholass-MacBook-Pro-2.local>
2019-12-01 14:13:23 -0600

    Updates

---
+++ precache-manifest.b277158225b8af55b4faf5a7725294a5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0dcd8ea3e4fa96df1c3bde299cf603",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "027d992d6daab4b713bc",
+    "url": "/stocks/static/js/2.f91f5e88.chunk.js"
+  },
+  {
+    "revision": "c2bd995a5a160d20fbf4",
+    "url": "/stocks/static/js/main.4420f58c.chunk.js"
+  },
+  {
+    "revision": "0aedf741f693fb0a45e5",
+    "url": "/stocks/static/js/runtime-main.fd54f26a.js"
+  }
+]);
